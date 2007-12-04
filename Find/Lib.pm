fe2ce5f23243e934847729d760f494cc7917cd34
Yann Kerherve <yann@sixapart.com>
2007-12-03 18:50:50 -0800

    removed one eval "";

--- Find/Lib.pm
+++ Find/Lib.pm
@@ -1,19 +1,14 @@
 package Find::Lib;
 use strict;
 use warnings;
+use lib;
 
 use File::Spec::Functions qw( catpath splitpath rel2abs catdir );
 sub import {
     my $class = shift;
     return unless @_;
     my $script = catpath( (splitpath( rel2abs $0 ))[ 0, 1 ], '' );
-    my $use_libs = '';
-    for (@_) {
-        my $libdir = catdir($script, $_);
-        $use_libs .= "use lib '$libdir';";
-    }
-    eval $use_libs;
-    warn $@ if $@;
+    lib->import( catdir($script, $_) ) for @_;
 }
 
 1;
