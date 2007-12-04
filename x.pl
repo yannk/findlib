97de90371bac838a057a5755833dc2f2728755eb
Yann Kerherve <yann@sixapart.com>
2007-12-03 19:10:46 -0800

    Added what's ugly and how it should be :)

--- x.pl
+++ x.pl
@@ -1,6 +1,18 @@
 use Find::Lib paths => [ 'x', 'x/y', 'x/../lib' ], 
               pkgs  => { 'Boot' => [ test => 1 ] }; 
 
+=cut
+Ugly now:
+
+use Find::Lib paths => [ "../core/lib "], pkgs => { "TypeCore::Bootstrap" => [ test => 1 ] }
+
+
+I want, simplified: one path + one bootstrap module
+Find::Lib '../core/lib' => 'TypeCore::Bootstrap' => [ tests => 1 ]
+
+Find::Lib '../core/lib' => 'TypeCore::Bootstrap', tests => 1;
+=cut
+
 use Foo;
 use Bar;
 use Baz;
